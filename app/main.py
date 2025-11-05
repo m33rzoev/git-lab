6190c188e830e972546adee4b28195fdfd349b88
Дилшод Мирзоев <mirzoev@MacBook-Pro-Dilsod.local>
2025-11-05 05:26:28 +0300

    Save changes before switching branches

--- app/main.py
+++ app/main.py
@@ -1,12 +1,1 @@
-print("App started")
-from utils import greet, save_note, load_notes
-
-if __name__ == "__main__":
-    print(greet("Student"))
-    save_note("Первый запуск приложения")
-    for i, note in enumerate(load_notes(), 1):
-        print(f"{i}. {note}")
-App initialized by person1
-New feature X developed by person1
-New change in dev1 for squash
-Conflicting change in main.py
+Conflicting change in main.py by person1
